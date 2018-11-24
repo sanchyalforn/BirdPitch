7a8ce753cd030dbbd49258053dd5de73e218d85c
carlotacb <carkbra@gmail.com>
2018-11-24 16:07:20 +0100

    Server up

---
+++ index.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let app = express();
+var port = process.env.PORT || 8080;
+var http = require('http').Server(app);
+var game = {};
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/static'));
+
+
+app.get('/', function (req, res) {
+    res.render('play', {});
+});
+
+app.listen(port, function () {
+     console.log("Equisde running on port " + port);
+});
