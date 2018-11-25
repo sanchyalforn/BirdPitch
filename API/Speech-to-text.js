a109fc83ebeeac448ceed1f99fc6cbcf45203f98
Sanchy <vsanchez.gassull@gmail.com>
2018-11-25 05:24:21 +0100

    COdi per la carlota

--- API/Speech-to-text.js
+++
@@ -1,7 +0,0 @@
-var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
-
-var speechToText = new SpeechToTextV1({
-    iam_apikey: 'vyqaNeNDKGT84Gm5EMBE3W2jbloWQnsjlQplm6HO8Pff',
-    url: 'https://stream-fra.watsonplatform.net/speech-to-text/api'
-});
-
